20913ebe18a20b4c41bf3cbea66a199ea129f543
liam <82913243+abythenice@users.noreply.github.com>
2025-12-10 05:48:06 -0800

    Enable 3D interactions and set transparent background for Spline scene

--- components/3DComponents/MiniHouse.jsx
+++ components/3DComponents/MiniHouse.jsx
@@ -1,9 +1,20 @@
 import "./Mini-3d-house.scss";
 import Spline from "@splinetool/react-spline";
+import { useCallback, useRef } from "react";
 
 const MiniHouse = () => {
-    return (
-    <Spline scene="https://prod.spline.design/xI6bQZSWJDbXKxvS/scene.splinecode" />
+  const splineRef = useRef();
+
+  const onLoad = useCallback((spline) => {
+    spline.setBackgroundColor("transparent");
+    splineRef.current = spline;
+  }, []);
+
+  return (
+    <Spline
+      scene="https://prod.spline.design/xI6bQZSWJDbXKxvS/scene.splinecode"
+      onLoad={onLoad}
+    />
   );
 };
 
